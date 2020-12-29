2c198e10009000aa8c59c669e202f14d941c00d2
Edu <edugacv@gmail.com>
2020-12-29 12:36:08 +0100

    Exercice done

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // Tu códgigo aquí
+let btn = document.querySelector('#get-joke')
+let displayJoke = document.querySelector('#display-joke')
+
+btn.addEventListener('click', updateJoke)
+
+function updateJoke(){
+    let xhttp = new XMLHttpRequest()
+    xhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            let jokes = JSON.parse(this.response)
+            displayJoke.innerHTML = jokes.value
+        }
+    }
+    xhttp.open('GET', 'https://api.chucknorris.io/jokes/random', true)
+        xhttp.send()
+}
