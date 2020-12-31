7fbfc6e9e398ca1d8ee81d25816293b7f0f3309b
bcn-alumno <bcn-alumno@nm_madrid.local>
2020-12-31 10:44:30 +0100

    Modified code and use fect insted of http request

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 // Tu códgigo aquí
-let btn = document.querySelector('#get-joke')
-let displayJoke = document.querySelector('#display-joke')
+let btn = document.querySelector("#get-joke");
+let displayJoke = document.querySelector("#display-joke");
 
-btn.addEventListener('click', updateJoke)
+btn.addEventListener("click", updateJoke);
 
-function updateJoke(){
-    let xhttp = new XMLHttpRequest()
-    xhttp.onreadystatechange = function(){
-        if(this.readyState == 4 && this.status == 200){
-            let jokes = JSON.parse(this.response)
-            displayJoke.innerHTML = jokes.value
-        }
-    }
-    xhttp.open('GET', 'https://api.chucknorris.io/jokes/random', true)
-        xhttp.send()
+// function updateJoke() {
+//   let xhttp = new XMLHttpRequest();
+//   xhttp.onreadystatechange = function () {
+//     if (this.readyState == 4 && this.status == 200) {
+//       let jokes = JSON.parse(this.response);
+//       displayJoke.innerHTML = jokes.value;
+//     }
+//   };
+//   xhttp.open("GET", "https://api.chucknorris.io/jokes/random", true);
+//   xhttp.send();
+// }
+
+//This does exactly the same as on top
+function updateJoke() {
+  fetch("https://api.chucknorris.io/jokes/random")
+    .then((response) => response.json())
+    .then((data) => (displayJoke.innerHTML = data.value));
 }
